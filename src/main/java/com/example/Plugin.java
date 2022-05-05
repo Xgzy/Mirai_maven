44c76a58b988b27b04a211a08d015f0eda8a1228
XGZ <XGZ@com>
2022-05-05 10:10:44 +0800

    基于mirai的Java模板

---
+++ src/main/java/com/example/Plugin.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+import net.mamoe.mirai.event.GlobalEventChannel;
+
+/**
+ * @author 17909
+ */
+public final class Plugin extends JavaPlugin {
+    public static final Plugin INSTANCE = new Plugin();
+
+    private Plugin() {
+        super(new JvmPluginDescriptionBuilder("org.example.plugin", "1.0-SNAPSHOT")
+                .name("MiraiBot")
+                .info("群管机器人和常用功能其他功能会陆续开发")
+                .author("Xgzy")
+                .build());
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin loaded!");
+    }
+
+}
