22725bac334c7be8862ebd37b23977018176dde2
Xgzy <67388089+Xgzy@users.noreply.github.com>
2022-05-10 14:54:05 +0800

    Update Plugin.java

--- src/main/java/com/example/Plugin.java
+++ src/main/java/com/example/Plugin.java
@@ -3,26 +3,24 @@ package com.example;
 
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
-import net.mamoe.mirai.event.GlobalEventChannel;
 
 /**
- * @author 17909
+ * @author XGZ
  */
 public final class Plugin extends JavaPlugin {
     public static final Plugin INSTANCE = new Plugin();
 
     private Plugin() {
-        super(new JvmPluginDescriptionBuilder("org.example.plugin", "1.0-SNAPSHOT")
+        super(new JvmPluginDescriptionBuilder("com.example.plugin", "1.0-SNAPSHOT")
                 .name("MiraiBot")
-                .info("群管机器人和常用功能其他功能会陆续开发")
-                .author("Xgzy")
+                .info("描述")
+                .author("作者")
                 .build());
     }
 
     @Override
     public void onEnable() {
         getLogger().info("Plugin loaded!");
-        GlobalEventChannel.INSTANCE.registerListenerHost(new MyEvent());
     }
 
 }
