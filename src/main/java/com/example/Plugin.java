b19f748a0165c8b814e4f030dcfa16d09ece5246
XGZ <XGZ@com>
2022-05-10 14:50:51 +0800

    把kotlin改成java

--- src/main/java/com/example/Plugin.java
+++ src/main/java/com/example/Plugin.java
@@ -1,25 +1,28 @@
 package com.example;
 
+
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+import net.mamoe.mirai.event.GlobalEventChannel;
 
 /**
- * @author XGZ
+ * @author 17909
  */
 public final class Plugin extends JavaPlugin {
     public static final Plugin INSTANCE = new Plugin();
 
     private Plugin() {
-        super(new JvmPluginDescriptionBuilder("com.example.plugin", "1.0")
+        super(new JvmPluginDescriptionBuilder("org.example.plugin", "1.0-SNAPSHOT")
                 .name("MiraiBot")
-                .info("描述")
-                .author("作者name")
+                .info("群管机器人和常用功能其他功能会陆续开发")
+                .author("Xgzy")
                 .build());
     }
 
     @Override
     public void onEnable() {
         getLogger().info("Plugin loaded!");
+        GlobalEventChannel.INSTANCE.registerListenerHost(new MyEvent());
     }
 
 }
